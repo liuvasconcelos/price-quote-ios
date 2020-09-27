c7c40f8e72138bb7a0d7b7a37f432899c4db0b98
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2020-09-27 19:09:30 -0300

    feature/#1 Remove Pods
    -
    
    Signed-off-by: Lívia Vasconcelos <liu.arquiteta@gmail.com>

--- Pods/Target Support Files/Pods-PriceQuoteIos/Pods-PriceQuoteIos-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PriceQuoteIos : NSObject
-@end
-@implementation PodsDummy_Pods_PriceQuoteIos
-@end
--- Pods/Target Support Files/Pods-PriceQuoteIosTests/Pods-PriceQuoteIosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PriceQuoteIosTests : NSObject
-@end
-@implementation PodsDummy_Pods_PriceQuoteIosTests
-@end
